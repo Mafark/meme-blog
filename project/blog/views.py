9586da129f17eaa1014866c5aeb493b4951b1d58
Никита Яблоков <danger@torman.ru>
2017-08-23 22:10:48 +0300

    Remove excess

--- project/blog/views.py
+++ project/blog/views.py
@@ -7,18 +7,8 @@ from .models import Meme
 # Create your views here.
 
 
-def index(request):
-    memes = get_list_or_404(Meme)
-    print(memes)
-    return HttpResponse("Hello, world. You're at the polls index.")
-
-
 class IndexView(generic.ListView):
     template_name = 'blog/index.html'
-    la = Meme.objects.order_by('-pub_date')[:5]
-    for l in la:
-        v = l.image
-        k = dir(l.image)
 
     def get_queryset(self):
         """Return the last five published questions."""
